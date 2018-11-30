db3cb0d390e0d241c473ea2c66e268c2ad2aac86
Sandeep Raj Kumbargeri <Sandeep.Kumbargeri@colorado.edu>
2018-11-30 14:51:30 +0000

    Update the ReadMe for Project Review

--- Project/README.md
+++ Project/README.md
@@ -1,18 +1,41 @@
-### Textures and lighting the scene in 3D - Dunkirk
+### CSCI 5229 Project - Chasing a UFO on the Moon
 
 Author: Sandeep Raj Kumbargeri
 CSCI 5229 - Computer Graphics - Fall 2018 - University of Colorado Boulder
 
 Elements taken from Willem A. (Vlakkies) Schreuder's [Ex8 - Objects](http://www.prinmath.com/csci5229/F18/programs/ex8.zip),
-[Ex9 - Projections](http://www.prinmath.com/csci5229/F18/programs/ex9.zip), [Ex13 - Lighting] (http://www.prinmath.com/csci5229/F18/programs/ex13.zip) and [Ex14 - Texture Mapping] (http://www.prinmath.com/csci5229/F18/programs/ex14.zip)
+[Ex9 - Projections](http://www.prinmath.com/csci5229/F18/programs/ex9.zip), [Ex13 - Lighting] (http://www.prinmath.com/csci5229/F18/programs/ex13.zip), [Ex14 - Texture Mapping] (http://www.prinmath.com/csci5229/F18/programs/ex14.zip), Ex17 - Draped Textures] (http://www.prinmath.com/csci5229/F18/programs/ex17.zip), Ex20 - Draped Textures] (http://www.prinmath.com/csci5229/F18/programs/ex20.zip) and lastly Ex25 - Thunderbirds] (http://www.prinmath.com/csci5229/F18/programs/ex25.zip).
 
-Displays a scene consisting of two textured ships and two textured airplanes and allows the user to explore in 3D. All the objects in the scene are lighted using a lightball which can be moved around and adjusted using the key bindings.
+Displays a scene consisting of a cubemap from the sruface of the moon with craters and mountains (draped textures) with stars and earth which can be seen on the top.
 
-#### How it works - Overview
-- This code is written using OpenGL API to draw objects and GLUT windowing API for window creation and resizing.
-- Special keyboard functions are used to map certain key presses to definite actions by registering key function callbacks.
-- Project function is used to switch between the orthogonal and prespective viewing modes and "display()" function is executed whenever "glutPostRedisplay()" is called, which generates the transformation matrix based on what is to be displayed and flushes the data to the screen.
-- The lighting is done locally at vertices using the normal vectors for that polygon plane.
+####Successfully implemented:
+
+- Cubemap (Moon surface, stars and earth).
+- Objects with textures and lighting (Ships, Spacecraft, UFO).
+- Mountains/Uneven-moon-surface with texture applied (Used draped textures and provided user-support to change the depth and position).
+- GIF/Video-Player (Implemented a virtual screen inside the scene, plays a small video (GIF image) in repeated playback fashion).
+- Cockpit view in both fly-mode and prespective modes.
+
+####Half-done:
+- Attempted to integrate and fully develop flight capability with the spacecraft. Able to traverse the world in the directio of the lorenz attractor, but unable to render the spacecraft yet.
+- Collision detection with the UFO. Once I'm able to move the spacecraft, the goal is to crash into the UFO and detect collisions in the background.
+
+####Not yet implemented:
+- Menu Items
+- Flag with wavy movement
+- Apollo Lander (stretch goal)
+
+#### Challenges Faced and "Gotcha!s"
+- The GIF-player seemed something very interesting to attempt. The goal was to use as less memory as possible. So, I used only one texture name and LoadTexBMP-ed to it with different textures for every 50ms to generate smooth image play-back. "This worked absolutely perfect!" is what I thought until I happened to find a segmentation fault. Upon observing the resource monitor and debugging using valgrind, there was a memory leak in the OpenGL calls. Need to dig more. Planning to use seperate texture names and then using them, if I don't find the memory leak.
+- The flight emulation demonstrated in the class uses DisplayLists which I'm not using. Rendering my spacecraft and have it first follow the Lorenz attractor path is my first goal. Then I'll figure out how to follow the mouse or respond to keyboard inputs.
+- Understanding the standard pipeline after the timer has been implementes has been a little challenging for me with idle, key, special functions calling glutPostRedisplay.
+
+#### Suggested review methodology
+- Compile and run the program using the following mentioned build instructions.
+- The fly-mode is turned on at the start-up and you will see the eye following lorenz's eye. The flight rendering is to be worked on.
+- Use the 'f/F' on the keyboard to turn off the fly-mode and you will enter prespective mode. This will show you play-back screen with the spacecraft and the UFO revolving aroung the spacecrat behind the screen. You could use 'b/B' on the keyboard to hide the screen.
+- Use the arrow keys to look around and observe the sky-cube, mountains and ships in the scene. Use 'Z/z' to increase and decrease the vertical exaggeration of the mountains.
+- Toggle the sky-cube using 'w/W' and this will show you the mountains completely.
 
 #### Build instructions
 - Run `make` in Windows, OS X and Linux. Download the make utility for Windows if needed.
@@ -33,12 +56,17 @@ Use arrow keys to change viewing angles
 - F8 - Change ball increment
 - F9 - Invert bottom normal
 - m - Toggles light movement
-- w/W - Toggles water
+- w/W - Toggles wskycube world
+- f/F - Fly mode toggle
 - [] - Lower/rise light
+- m/M - Toggle move (pause and un-pause movement in the scene)
+- q/Q - Toggle Cockpit
+- b/B - Toggle Playback Screen
 - p - Toggles ortogonal/perspective projection
 - +/- - Change field of view of perspective
 - x - Toggle axes
 - arrows - Change view angle
+- Z/z - Increase/Decrease vertical exaggeration
 - PgDn/PgUp - Zoom in and out
 - 0 - Reset view angle
 - ESC - Exit
--- Project/main.c
+++ Project/main.c
@@ -1,55 +1,78 @@
 /*
- *  Textures and lighting the scene in 3D - Dunkirk
- *
- *  Author: Sandeep Raj Kumbargeri
- *  CSCI 5229 - Computer Graphics - Fall 2018 - University of Colorado Boulder
- *
- *  Elements taken from Willem A. (Vlakkies) Schreuder's "Ex8 - Objects" (http://www.prinmath.com/csci5229/F18/programs/ex8.zip),
- *  "Ex9 - Projections" (http://www.prinmath.com/csci5229/F18/programs/ex9.zip), "Ex13 - Lighting" (http://www.prinmath.com/csci5229/F18/programs/ex13.zip)
- *  and "Ex14 - Texture Mapping" (http://www.prinmath.com/csci5229/F18/programs/ex14.zip)
- *
- *  Displays a scene consisting of two textured ships and two textured airplanes and allows the user to explore in 3D. All the objects in the scene are
- *  lighted using a lightball which can be moved around and adjusted using the key bindings.
- *
- *  How it works - Overview:
- *  - This code is written using OpenGL API to draw objects and GLUT windowing API for window creation and resizing.
- *  - Special keyboard functions are used to map certain key presses to definite actions by registering key function callbacks.
- *  - Project function is used to switch between the orthogonal and prespective viewing modes and "display()" function is executed
- *  - whenever "glutPostRedisplay()" is called, which generates the transformation matrix based on what is to be displayed and flushes
- *  - the data to the screen. The lighting is done locally at vertices using the normal vectors for that polygon plane.
- *
- *  Build instructions:
- *  Run "make" in Windows, OS X and Linux. Download the make utility for Windows if needed.
- *  Run "make clean" to clean up the generated files.
- *
- *  Use arrow keys to change viewing angles
- *
- *  Special Key Bindings:
- *  Key bindings:
- *  l          Toggles lighting
- *  a/A        Decrease/increase ambient light
- *  d/D        Decrease/increase diffuse light
- *  s/S        Decrease/increase specular light
- *  e/E        Decrease/increase emitted light
- *  n/N        Decrease/increase shininess
- *  F1         Toggle smooth/flat shading
- *  F2         Toggle local viewer mode
- *  F3         Toggle light distance (1/5)
- *  F8         Change ball increment
- *  F9         Invert bottom normal
- *  m          Toggles light movement
- *  w/W        Toggles water
- *  []         Lower/rise light
- *  p          Toggles ortogonal/perspective projection
- *  +/-        Change field of view of perspective
- *  x          Toggle axes
- *  Z/z        Increase/Decrease vertical exaggeration
- *  arrows     Change view angle
- *  PgDn/PgUp  Zoom in and out
- *  0          Reset view angle
- *  ESC        Exit
- */
+### CSCI 5229 Project - Chasing a UFO on the Moon
 
+Author: Sandeep Raj Kumbargeri
+CSCI 5229 - Computer Graphics - Fall 2018 - University of Colorado Boulder
+
+Elements taken from Willem A. (Vlakkies) Schreuder's [Ex8 - Objects](http://www.prinmath.com/csci5229/F18/programs/ex8.zip),
+[Ex9 - Projections](http://www.prinmath.com/csci5229/F18/programs/ex9.zip), [Ex13 - Lighting] (http://www.prinmath.com/csci5229/F18/programs/ex13.zip), [Ex14 - Texture Mapping] (http://www.prinmath.com/csci5229/F18/programs/ex14.zip), Ex17 - Draped Textures] (http://www.prinmath.com/csci5229/F18/programs/ex17.zip), Ex20 - Draped Textures] (http://www.prinmath.com/csci5229/F18/programs/ex20.zip) and lastly Ex25 - Thunderbirds] (http://www.prinmath.com/csci5229/F18/programs/ex25.zip).
+
+Displays a scene consisting of a cubemap from the sruface of the moon with craters and mountains (draped textures) with stars and earth which can be seen on the top.
+
+####Successfully implemented:
+
+- Cubemap (Moon surface, stars and earth).
+- Objects with textures and lighting (Ships, Spacecraft, UFO).
+- Mountains/Uneven-moon-surface with texture applied (Used draped textures and provided user-support to change the depth and position).
+- GIF/Video-Player (Implemented a virtual screen inside the scene, plays a small video (GIF image) in repeated playback fashion).
+- Cockpit view in both fly-mode and prespective modes.
+
+####Half-done:
+- Attempted to integrate and fully develop flight capability with the spacecraft. Able to traverse the world in the directio of the lorenz attractor, but unable to render the spacecraft yet.
+- Collision detection with the UFO. Once I'm able to move the spacecraft, the goal is to crash into the UFO and detect collisions in the background.
+
+####Not yet implemented:
+- Menu Items
+- Flag with wavy movement
+- Apollo Lander (stretch goal)
+
+#### Challenges Faced and "Gotcha!s"
+- The GIF-player seemed something very interesting to attempt. The goal was to use as less memory as possible. So, I used only one texture name and LoadTexBMP-ed to it with different textures for every 50ms to generate smooth image play-back. "This worked absolutely perfect!" is what I thought until I happened to find a segmentation fault. Upon observing the resource monitor and debugging using valgrind, there was a memory leak in the OpenGL calls. Need to dig more. Planning to use seperate texture names and then using them, if I don't find the memory leak.
+- The flight emulation demonstrated in the class uses DisplayLists which I'm not using. Rendering my spacecraft and have it first follow the Lorenz attractor path is my first goal. Then I'll figure out how to follow the mouse or respond to keyboard inputs.
+- Understanding the standard pipeline after the timer has been implementes has been a little challenging for me with idle, key, special functions calling glutPostRedisplay.
+
+#### Suggested review methodology
+- Compile and run the program using the following mentioned build instructions.
+- The fly-mode is turned on at the start-up and you will see the eye following lorenz's eye. The flight rendering is to be worked on.
+- Use the 'f/F' on the keyboard to turn off the fly-mode and you will enter prespective mode. This will show you play-back screen with the spacecraft and the UFO revolving aroung the spacecrat behind the screen. You could use 'b/B' on the keyboard to hide the screen.
+- Use the arrow keys to look around and observe the sky-cube, mountains and ships in the scene. Use 'Z/z' to increase and decrease the vertical exaggeration of the mountains.
+- Toggle the sky-cube using 'w/W' and this will show you the mountains completely.
+
+#### Build instructions
+- Run `make` in Windows, OS X and Linux. Download the make utility for Windows if needed.
+- Run `make clean` to clean up the generated files.
+
+Use arrow keys to change viewing angles
+
+#### Special Key Bindings
+- l - Toggles lighting
+- a/A - Decrease/increase ambient light
+- d/D - Decrease/increase diffuse light
+- s/S - Decrease/increase specular light
+- e/E - Decrease/increase emitted light
+- n/N - Decrease/increase shininess
+- F1 - Toggle smooth/flat shading
+- F2 - Toggle local viewer mode
+- F3 - Toggle light distance (1/5)
+- F8 - Change ball increment
+- F9 - Invert bottom normal
+- m - Toggles light movement
+- w/W - Toggles wskycube world
+- f/F - Fly mode toggle
+- [] - Lower/rise light
+- m/M - Toggle move (pause and un-pause movement in the scene)
+- q/Q - Toggle Cockpit
+- b/B - Toggle Playback Screen
+- p - Toggles ortogonal/perspective projection
+- +/- - Change field of view of perspective
+- x - Toggle axes
+- arrows - Change view angle
+- Z/z - Increase/Decrease vertical exaggeration
+- PgDn/PgUp - Zoom in and out
+- 0 - Reset view angle
+- ESC - Exit
+
+*/
 
 #include "CSCIx229.h"
 #include <stdbool.h>
@@ -929,10 +952,10 @@ void display()
   if(show_pb_screen)
     playback_screen(0, 0, 50, 2, 1, 0, 0, 0, "textures/playback/playback", 132);
 
-   draw_mountains(0, -64, -40, 0.1250, 0.0625, 0.0625, 270, 0, 180, 10);
-   draw_mountains(40, -64, 0, 0.1250, 0.0625, 0.0625, 270, 0, 90, 4);
-   draw_mountains(0, -64, 40, 0.1250, 0.0625, 0.0625, 270, 0, 0, 7);
-   draw_mountains(-40, -64, 0, 0.1250, 0.0625, 0.0625, 270, 0, 270, 3);
+   draw_mountains(0, -64, -40, 0.1250, 0.0625, 0.0625, 270, 0, 180, zmag + 5);
+   draw_mountains(40, -64, 0, 0.1250, 0.0625, 0.0625, 270, 0, 90, zmag - 1);
+   draw_mountains(0, -64, 40, 0.1250, 0.0625, 0.0625, 270, 0, 0, zmag + 2);
+   draw_mountains(-40, -64, 0, 0.1250, 0.0625, 0.0625, 270, 0, 270, zmag - 3);
 
   //Draw the onjects
   draw_ship(0,-64,-10,4,4,4,0,90,0);
